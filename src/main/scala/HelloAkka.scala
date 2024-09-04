0ba53a6aaa16e95915b8b615824b0387673fb38e
larinpaul <www.larinpavel@mail.ru>
2024-09-03 19:42:07 -0700

    // Define Actor Messages case class WhoToGreet(who: String)

--- src/main/scala/HelloAkka.scala
+++ src/main/scala/HelloAkka.scala
@@ -3,8 +3,11 @@ class HelloAkka {
 }
 
 // Define Actor Messages
+case class WhoToGreet(who: String)
 
 // Define Greeter Actor
+// our receive function is a parcial function that takes anything and returns a unit
+// As we have a single message, we'll have a single case
 
 object HelloAkkScala extends App {
 
