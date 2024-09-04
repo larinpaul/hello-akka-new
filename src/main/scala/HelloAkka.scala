c86e622e3ec7eefcf52eb59d7aa6a89610b3e5ee
larinpaul <www.larinpavel@mail.ru>
2024-09-03 19:39:57 -0700

    // Define Actor Messages
    // Define Greeter Actor
    
    object HelloAkkScala extends App {
    
      // Create the 'hello akka' actor system
    
      // Create the 'greeter' actor
    
      // Send WhoToGreet Message to actor
    
    }

---
+++ src/main/scala/HelloAkka.scala
@@ -0,0 +1,19 @@
+class HelloAkka {
+
+}
+
+// Define Actor Messages
+
+// Define Greeter Actor
+
+object HelloAkkScala extends App {
+
+  // Create the 'hello akka' actor system
+
+  // Create the 'greeter' actor
+
+  // Send WhoToGreet Message to actor
+
+}
+
+
