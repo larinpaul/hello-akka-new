0eb9fec7af2b8e60075cf547e0976fde64f77ecc
larinpaul <www.larinpavel@mail.ru>
2024-09-03 19:37:24 -0700

    // To create a Hello World App, We Need to...
        // * Create Greet message
        // * Create Greeter actor
        // * Send Greet message to your Greeter actor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,5 +3,11 @@ object Main {
 
     println("Hello world!")
 
+    // To create a Hello World App, We Need to...
+
+    // * Create Greet message
+    // * Create Greeter actor
+    // * Send Greet message to your Greeter actor
+
   }
 }
