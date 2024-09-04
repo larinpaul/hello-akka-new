427d0f3c196c4f9fde6f1a0669d8c53c9e1cb875
larinpaul <www.larinpavel@mail.ru>
2024-09-03 19:50:29 -0700

    Moved everything into the Main.scala file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,3 +11,35 @@ object Main {
 
   }
 }
+
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+class HelloAkka {
+
+}
+
+// Define Actor Messages
+case class WhoToGreet(who: String)
+
+// Define Greeter Actor
+// our receive function is a parcial function that takes anything and returns a unit
+// As we have a single message, we'll have a single case
+class Greeter extends Actor {
+  def receive = {
+    case WhoToGreet(who) => println(s"Hello $who")
+  }
+}
+
+object HelloAkkScala extends App {
+
+  // Create the 'hello akka' actor system
+  val system = ActorSystem("Hello-Akka")
+
+  // Create the 'greeter' actor
+  val greeter = system.actorOf(Props[Greeter], "greeter")
+
+  // Send WhoToGreet Message to actor
+  greeter ! WhoToGreet("Akka")
+
+}
