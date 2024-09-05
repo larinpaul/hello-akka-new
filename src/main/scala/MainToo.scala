05ded0b9df06d5fe16d5e335c1ef22d163277aba
larinpaul <www.larinpavel@mail.ru>
2024-09-05 07:58:18 -0700

    // Part 3.3. Replacing Actor Behavior via become/unbecome
        // * How to change actor behavior at runtime by become and unbecome
        // * Stash messages
    
        // We will first implement UserStorage Actor

---
+++ src/main/scala/MainToo.scala
@@ -0,0 +1,14 @@
+object MainToo {
+
+  def main(args: Array[String]): Unit = {
+
+    // Part 3.3. Replacing Actor Behavior via become/unbecome
+
+    // * How to change actor behavior at runtime by become and unbecome
+    // * Stash messages
+
+    // We will first implement UserStorage Actor
+
+  }
+
+}
