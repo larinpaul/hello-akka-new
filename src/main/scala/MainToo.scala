81c9fbaca90d290e2d085151d62a3161d146d2f7
larinpaul <www.larinpavel@mail.ru>
2024-09-05 08:47:48 -0700

    // package com.packt.akka
    import UserStorage.{Connect, Operation}
    import akka.actor.{Actor, ActorRef, ActorSystem, Props, Stash}
    import akka.io.UdpConnected.Disconnect
    
    case class User(username: String, email: String)
    
    object UserStorage {
    
      trait DBOperation
      object DBOperation {
        case object Create extends DBOperation
        case object Update extends DBOperation
        case object Read extends DBOperation
        case object Delete extends DBOperation
      }
    
      case object Connect
      case object DisConnect
      case class Operation(dBOperation: DBOperation, user: Option[User])
    
    }
    
    class UserStorage extends Actor {
    
      def receive = disconnected
    
      def connected: Actor.Receive = {
        case Disconnect =>
          println("User Storage Disconnect from DB")
          context.unbecome()
        case Operation(op, user) =>
          println(s"User Storage receive ${op} to do in user: ${user}")
      }
    
      def disconnected: Actor.Receive = {
        case Connect =>
            println(s"User Storage connected to DB")
            context.become()
      }
    
    }
    
    object BecomeHotswap extends App {
      import UserStorage._
    
      val system = ActorSystem("Hotswap-Become")
    
      val userStorage = system.actorOf(Props[UserStorage], "userStorage")
    
      userStorage ! Connect
    
      userStorage ! Operation(DBOperation.Create, Some(User("Admin", "admin@packt.com")))
    
      userStorage ! Disconnect
    
      Thread.sleep(100)
    
      system.shutdown()
    }

--- src/main/scala/MainToo.scala
+++ src/main/scala/MainToo.scala
@@ -1,21 +1,3 @@
-object MainToo {
-
-  def main(args: Array[String]): Unit = {
-
-    // Part 3.3. Replacing Actor Behavior via become/unbecome
-
-    // * How to change actor behavior at runtime by become and unbecome
-    // * Stash messages
-
-    // We will first implement UserStorage Actor
-    // Out Actor should be able to:
-    // * Connect to database
-    // * Disconnect from database
-    // * CRUD operations
-  }
-
-}
-
 // package com.packt.akka
 
 import UserStorage.{Connect, Operation}
