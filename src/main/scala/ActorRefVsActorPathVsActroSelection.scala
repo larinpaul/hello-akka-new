55fe3ae41ec5ce4c14d70a28bca709d52a5f46fb
larinpaul <www.larinpavel@mail.ru>
2024-09-07 22:59:02 -0700

    // package com.packt.akka
    import akka.actor.{ ActorRef, ActorSystem, Props, Actor, Identify, ActorIdentity }
    
    class Watcher extends Actor {
    
      car counterRef: ActorRef =
      val selection = context.actorSelection("/user/counter")
      def receive = {
        case ActorIdentity(_, Some(ref)) =>
          println(s"Actor Reference for counter is ${ref}")
        case ActorIdentity(_, None) =>
          println("Actor selection for actor doesn't live :(")
      }
    
    }

--- src/main/scala/ActorRefVsActorPathVsActroSelection.scala
+++ src/main/scala/ActorRefVsActorPathVsActroSelection.scala
@@ -74,3 +74,21 @@ object ActorPath extends App {
 
 }
 
+// package com.packt.akka
+
+import akka.actor.{ ActorRef, ActorSystem, Props, Actor, Identify, ActorIdentity }
+
+class Watcher extends Actor {
+
+  var counterRef: ActorRef = _
+  val selection = context.actorSelection("/user/counter")
+  def receive = {
+    case ActorIdentity(_, Some(ref)) =>
+      println(s"Actor Reference for counter is ${ref}")
+    case ActorIdentity(_, None) =>
+      println("Actor selection for actor doesn't live : (")
+  }
+
+
+}
+
