55b447a4b3cbee47666f941b58409df3c134e12f
larinpaul <www.larinpavel@mail.ru>
2024-09-08 19:46:21 -0700

    // Let's look at the Worker actor, // which only handles work messages
    // package com.packt.akka
    
    import akka.actor.Actor
    
    class Worker extends Actor {
      import Worker._
    
      def receive = {
        case msg: Work =>
          println(s"I received Work Message and My ActorRef: ${self}")
      }
    }
    
    object Worker {
      case class Work()
    }

--- src/main/scala/SendingMessagesViaRouter.scala
+++ src/main/scala/SendingMessagesViaRouter.scala
@@ -11,3 +11,23 @@ class SendingMessagesViaRouter {
 // * Implementing some Akka routing strategies
 // * Introducing other router strategies
 
+
+// Let's look at the Worker actor,
+// which only handles work messages
+
+// package com.packt.akka
+
+import akka.actor.Actor
+
+class Worker extends Actor {
+  import Worker._
+
+  def receive = {
+    case msg: Work =>
+      println(s"I received Work Message and My ActorRef: ${self}")
+  }
+}
+
+object Worker {
+  case class Work()
+}
