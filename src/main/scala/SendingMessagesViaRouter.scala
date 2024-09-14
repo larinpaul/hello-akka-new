0788a3f78e56dae1354b6f1362d3cee27ec0ef53
larinpaul <www.larinpavel@mail.ru>
2024-09-13 22:06:17 -0700

    object Random extends App {
      val system = ActorSystem("Random-Router")
    
      // /.../
    
      system.actorOf(Props[Worker], "w1")
      system.actorOf(Props[Worker], "w2")
      system.actorOf(Props[Worker], "w3")
    
      val paths = List("/user/w1", "user/w2", "/user/w3")
    
      val routerGroup = system.actorOf(RandomGroup(paths).props(), "random-router-group")
      routerGroup ! Work()
      routerGroup ! Work()
      routerGroup ! Work()
      routerGroup ! Work()
      system.terminate()
    
    }

--- src/main/scala/SendingMessagesViaRouter.scala
+++ src/main/scala/SendingMessagesViaRouter.scala
@@ -19,7 +19,7 @@ class SendingMessagesViaRouter {
 
 import Worker.Work
 import akka.actor.{Actor, ActorRef, Props}
-import akka.routing.FromConfig
+import akka.routing.{FromConfig, RandomGroup}
 
 class Worker extends Actor {
   import Worker._
@@ -127,5 +127,25 @@ object RandomFirst extends App {
 
 }
 
+object Random extends App {
+
+  val system = ActorSystem("Random-Router")
+
+  // /.../
+
+  system.actorOf(Props[Worker], "w1")
+  system.actorOf(Props[Worker], "w2")
+  system.actorOf(Props[Worker], "w3")
+
+  val paths = List("/user/w1", "user/w2", "/user/w3")
+
+  val routerGroup = system.actorOf(RandomGroup(paths).props(), "random-router-group")
+  routerGroup ! Work()
+  routerGroup ! Work()
+  routerGroup ! Work()
+  routerGroup ! Work()
+  system.terminate()
+
+}
 
 
