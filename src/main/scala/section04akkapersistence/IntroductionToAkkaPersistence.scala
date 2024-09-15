b06b2fbf76225ee483e358f9f192925321d0d78f
larinpaul <www.larinpavel@mail.ru>
2024-09-15 05:57:02 -0700

    // In this Section, we are going to take a look at...   // * Introduction to Akka Persistence   // * Creating Persistent Actor   // * Playing with Persistent Actor   // * Persistent FSM   // * Persistence Query
      // Introduction to Akka Persistence
      // * Overview of Akka Persistence
      // * Architecture of Akka Persistence`
    
      // Snapshot store
      // Journal
      // Persistent View -> Journal
      // Persistent Actor -> Journal
      // Persistent View -> Snapshot store
      // Persistent Actor -> Snapshot store
    
      // Full documentation for persistence:
      // doc.akka.io/docs/akka/snapshot/scala/actors.html#Become_Unbecome
      // https://github.com/krasserm/akka-persistence-cassandra/

---
+++ src/main/scala/section04akkapersistence/IntroductionToAkkaPersistence.scala
@@ -0,0 +1,28 @@
+package section04akkapersistence
+
+class IntroductionToAkkaPersistence {
+
+  // In this Section, we are going to take a look at...
+  // * Introduction to Akka Persistence
+  // * Creating Persistent Actor
+  // * Playing with Persistent Actor
+  // * Persistent FSM
+  // * Persistence Query
+
+
+  // Introduction to Akka Persistence
+  // * Overview of Akka Persistence
+  // * Architecture of Akka Persistence`
+
+  // Snapshot store
+  // Journal
+  // Persistent View -> Journal
+  // Persistent Actor -> Journal
+  // Persistent View -> Snapshot store
+  // Persistent Actor -> Snapshot store
+
+  // Full documentation for persistence:
+  // doc.akka.io/docs/akka/snapshot/scala/actors.html#Become_Unbecome
+  // https://github.com/krasserm/akka-persistence-cassandra/
+
+}
