1a217a3392d146ff3da0fbcc366ada4f4db6e878
larinpaul <www.larinpavel@mail.ru>
2024-09-16 19:43:12 -0700

    Still not working...
    package section04akkapersistence
    
    import akka.actor.Actor.Receive
    import akka.actor.ActorLogging
    
    class CreatingPersistentActors {
    
      // Part 4.2
      // Creating Persistent Actors
      // * Define Counter Actor
      // * Implement Counter Actor
    
    }
    
    // package com.packt.akka
    
    object Counter {
      sealed trait Operation {
        val count: Int
      }
    
      case class Increment(override val count: Int) extends Operation
      case class Decrement(override val count: Int) extends Operation
    
      case class Cmd(op: Operation)
      case class Evt(op: Operation)
    
      case class State(count: Int)
    
    }
    
    object Counter {
      sealed trait Operation {
        val count: Int
      }
    
      case class Increment(override val count: Int) extends Operation
      case class Decrement(override val count: Int) extends Operation
    
      case class Cmd(op: Operation)
      case class Evt(op: Operation)
    
      case class State(count: Int)
    }
    
    class Counter extends PersistentActor with ActorLogging {
      import Counter._
    
      println("Starting ........................")
    
      // Persistent Identifier
      override def persistenceId: String = "counter-example"
    
      var state: State = State(count = 0)
    
      def updateState(evt: Evt): Unit = evt match {
        case Evt(Increment(count)) =>
          state = State(count = state.count + count)
        case Evt(Decrement(count)) =>
          state = State(count = state.count - count)
      }
    
      // Persistent receive on recovery mood
      override def receiveRecover: Receive = {
        case evt: Evt =>
          println(s"Counter receive ${evt} on recovering mood")
          updateState(evt)
        case SnapshotOffer(_, snapshot: State) =>
          println(s"Counter receive snapshot with data: ${snapshot} on recovering mood")
          state = snapshot
      }
    
      // Persistent receive on normal mood
      override def receiveCommand: Receive = {
        case cmd @ Cmd(op) =>
          println(s"Counter receive ${cmd}")
          persist(Evt(op)) { evt =>
            updateState(evt)
          }
    
        case "print" =>
          println(s"The Current state of counter is ${state}")
      }
    }
    
    class Counter extends PersistentActor with ActorLogging {
      import Counter._
    
      println("Starting ........................")
    
      // Persistent Identifier
      override def persistenceId = "counter-example"
    
      var state: State = State(count= 0)
    
      def updateState(evt: Evt): Unit = evt match {
        case Evt(Increment(count)) =>
            state = State(count = state.count + count)
        case Evt(Decrement(count)) =>
          state = State(count = state.count - count)
      }
    
      // Persistent receive on recovery mood
      val receiveRecover: Receive = {
        case evt: Evt =>
          println(s"Counter receive ${evt} on recovering mood")
          updateState(evt)
        case SnapshotOffer(_, snapshot: State) =>
          println(s"Counter receive snapshot with data: ${snapshot} on recovering mood")
          state = snapshot
      }
    
      // Persistent receive on normal mood
      val receiveCommand: Receive = {
        case cmd @ Cmd(op) =>
          println(s"Counter receive ${cmd}")
          persist(Evt(op)) { evt =>
            updateState(evt)
          }
    
        case "print" =>
          println(s"The Current state of counter is ${state}")
      }
    
    }

--- src/main/scala/section04akkapersistence/CreatingPersistentActors.scala
+++ src/main/scala/section04akkapersistence/CreatingPersistentActors.scala
@@ -29,6 +29,60 @@ object Counter {
 
 }
 
+object Counter {
+  sealed trait Operation {
+    val count: Int
+  }
+
+  case class Increment(override val count: Int) extends Operation
+  case class Decrement(override val count: Int) extends Operation
+
+  case class Cmd(op: Operation)
+  case class Evt(op: Operation)
+
+  case class State(count: Int)
+}
+
+class Counter extends PersistentActor with ActorLogging {
+  import Counter._
+
+  println("Starting ........................")
+
+  // Persistent Identifier
+  override def persistenceId: String = "counter-example"
+
+  var state: State = State(count = 0)
+
+  def updateState(evt: Evt): Unit = evt match {
+    case Evt(Increment(count)) =>
+      state = State(count = state.count + count)
+    case Evt(Decrement(count)) =>
+      state = State(count = state.count - count)
+  }
+
+  // Persistent receive on recovery mood
+  override def receiveRecover: Receive = {
+    case evt: Evt =>
+      println(s"Counter receive ${evt} on recovering mood")
+      updateState(evt)
+    case SnapshotOffer(_, snapshot: State) =>
+      println(s"Counter receive snapshot with data: ${snapshot} on recovering mood")
+      state = snapshot
+  }
+
+  // Persistent receive on normal mood
+  override def receiveCommand: Receive = {
+    case cmd @ Cmd(op) =>
+      println(s"Counter receive ${cmd}")
+      persist(Evt(op)) { evt =>
+        updateState(evt)
+      }
+
+    case "print" =>
+      println(s"The Current state of counter is ${state}")
+  }
+}
+
 class Counter extends PersistentActor with ActorLogging {
   import Counter._
 
