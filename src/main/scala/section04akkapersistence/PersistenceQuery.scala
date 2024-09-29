5f80e96048a6d985f15684d2d05d47c2ad1a6340
larinpaul <www.larinpavel@mail.ru>
2024-09-29 12:13:46 +0500

    // Part 4.5   // Persistence Query
      * CQRS Pattern = Command Query Responsibility Segregation // Separates Read and Write Operations
    
      * Introduction to Persistence Query
      * Implement system to view account tags

--- src/main/scala/section04akkapersistence/PersistenceQuery.scala
+++ src/main/scala/section04akkapersistence/PersistenceQuery.scala
@@ -5,6 +5,13 @@ class PersistenceQuery {
   // Part 4.5
   // Persistence Query
 
+  // * CQRS Pattern // Command Query Responsibility Segregation // Separates Read and Write operations
+  // * Introduction to Persistence Query
+  // * Implement system to view account logs
+
+
+
+
 }
 
 
