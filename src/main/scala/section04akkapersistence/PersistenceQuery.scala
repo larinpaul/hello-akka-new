61b60e1d9b35045df923162bd52ebcb09f03eab8
larinpaul <www.larinpavel@mail.ru>
2024-09-29 12:17:00 +0500

    ** Read events for specific Persistent Actor * Events-By-Persistence-Id * Current-Events-By-Persistence-Id
    * Read all events based on Tags
    * Events-By-Tag
    * Current-Events-By-Tag

--- src/main/scala/section04akkapersistence/PersistenceQuery.scala
+++ src/main/scala/section04akkapersistence/PersistenceQuery.scala
@@ -9,9 +9,18 @@ class PersistenceQuery {
   // * Introduction to Persistence Query
   // * Implement system to view account logs
 
+  // Predefined Queries
 
+  // ** Read events for specific Persistent Actor
+  // * Events-By-Persistence-Id
+  // * Current-Events-By-Persistence-Id
 
+  // * Read all events based on Tags
+  // * Events-By-Tag
+  // * Current-Events-By-Tag
 
 }
+
+// We will implement a view from our previous persistence actor
 
 
