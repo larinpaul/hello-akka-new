cce3315cd6ef0a51ec743f00cd835ab5c7888200
larinpaul <www.larinpavel@mail.ru>
2024-10-01 07:31:31 +0500

    // In this Section, we are going to take a look at...     // * Akka Remoting     // * Akka Cluster     // * Load balancer     // * Cluster Sharding     // * Cluster Singleton

---
+++ src/main/scala/section05playingwithremoteactors/PlayingWithRemoteActorsLearningAkka.java
@@ -0,0 +1,12 @@
+package section05playingwithremoteactors;
+
+public class PlayingWithRemoteActorsLearningAkka {
+
+    // In this Section, we are going to take a look at...
+    // * Akka Remoting
+    // * Akka Cluster
+    // * Load balancer
+    // * Cluster Sharding
+    // * Cluster Singleton
+
+}
