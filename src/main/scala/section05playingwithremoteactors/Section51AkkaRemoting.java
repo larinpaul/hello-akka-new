f9261f7b6350828e49014c47e89ea49d1628d04e
larinpaul <www.larinpavel@mail.ru>
2024-10-02 08:54:08 +0500

    // * Introduction to Akka Remoting     // * Preparing your Actor System for Remoting     // * Playing with remote actors
        // * We worked on examples that have one Actor System running on local machine
        // But usually there are more
        // * Everything in Akka is designed to work in a distributed setting
        // * Akka Remoting is a communication module
        // In production, it is better to rely on akka cluster rather than akka remoting
        // However let's see how it works before we jump into Akka cluster

---
+++ src/main/scala/section05playingwithremoteactors/Section51AkkaRemoting.java
@@ -0,0 +1,16 @@
+package section05playingwithremoteactors;
+
+public class Section51AkkaRemoting {
+
+    // * Introduction to Akka Remoting
+    // * Preparing your Actor System for Remoting
+    // * Playing with remote actors
+
+    // * We worked on examples that have one Actor System running on local machine
+    // But usually there are more
+    // * Everything in Akka is designed to work in a distributed setting
+    // * Akka Remoting is a communication module
+    // In production, it is better to rely on akka cluster rather than akka remoting
+    // However let's see how it works before we jump into Akka cluster
+
+}
