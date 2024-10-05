757f276d6cb9d40a2a0616c148752971c97c9be1
larinpaul <www.larinpavel@mail.ru>
2024-10-05 14:27:16 +0500

    object MemberServiceLookup extends App {   val config = ConfigFactory.load.getConfig("MemberServiceLookup")   val system = ActorSystem("MemberServiceLookup", config)   val worker = system.actorSelection("akka.tcp://MembersService@127.0.0.1:2552/user/remote-worker")   worker ! Worker.Work("Hi Remote Actor") }

--- src/main/scala/section05playingwithremoteactors/Remote.scala
+++ src/main/scala/section05playingwithremoteactors/Remote.scala
@@ -22,3 +22,10 @@ object MembersService extends App {
   println(s"Worker actor path is ${worker.path}")
 }
 
+object MemberServiceLookup extends App {
+  val config = ConfigFactory.load.getConfig("MemberServiceLookup")
+  val system = ActorSystem("MemberServiceLookup", config)
+  val worker = system.actorSelection("akka.tcp://MembersService@127.0.0.1:2552/user/remote-worker")
+  worker ! Worker.Work("Hi Remote Actor")
+}
+
