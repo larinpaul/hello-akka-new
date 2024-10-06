f93ac0788905e84d03876861b532626bcc191d05
larinpaul <www.larinpavel@mail.ru>
2024-10-06 11:51:45 +0500

    // Akka Cluster   // * Akka Cluster provides a fault-tolerant decentralized peer-to-peer cluster   // membership service with no single point of failure or single point of bottleneck   // * Akka Cluster does this using gossip protocol and an automatic failure detector

---
+++ src/main/scala/section05playingwithremoteactors/Section52BuildingAClusterLearningAkka.scala
@@ -0,0 +1,10 @@
+package section05playingwithremoteactors
+
+class Section52BuildingAClusterLearningAkka {
+
+  // Akka Cluster
+  // * Akka Cluster provides a fault-tolerant decentralized peer-to-peer cluster
+  // membership service with no single point of failure or single point of bottleneck
+  // * Akka Cluster does this using gossip protocol and an automatic failure detector
+
+}
