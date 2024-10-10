78a2a56ef8b0914f901734613f3d7941d2151446
larinpaul <www.larinpavel@mail.ru>
2024-10-10 08:24:31 +0500

    // package com.packt.akka.cluster
    // import ...
    
    class Backend extends Actor {
    
      val cluster = ???
    
      // subscribe to cluster changes, MemberUp
      // re-subscribe when restart
      override def preStart(): Unit = ???
    
      override def postStop(): Unit = ???
    
      def receive = {
        case Add(num1, num2) =>
          println(s"I'm a backend with path: ${self} and I received add operation.")
      }
    
    }
    
    object Backend {}

--- src/main/scala/section05playingwithremoteactors/Section52BuildingAClusterLearningAkka.scala
+++ src/main/scala/section05playingwithremoteactors/Section52BuildingAClusterLearningAkka.scala
@@ -1,5 +1,7 @@
 package section05playingwithremoteactors
 
+import akka.actor.Actor
+
 class Section52BuildingAClusterLearningAkka {
 
   // Akka Cluster
@@ -16,3 +18,27 @@ class Section52BuildingAClusterLearningAkka {
   //
 
 }
+
+// package com.packt.akka.cluster
+
+// import ...
+
+class Backend extends Actor {
+
+  val cluster = ???
+
+  // subscribe to cluster changes, MemberUp
+  // re-subscribe when restart
+  override def preStart(): Unit = ???
+
+  override def postStop(): Unit = ???
+
+  def receive = {
+    case Add(num1, num2) =>
+      println(s"I'm a backend with path: ${self} and I received add operation.")
+  }
+
+}
+
+object Backend {}
+
