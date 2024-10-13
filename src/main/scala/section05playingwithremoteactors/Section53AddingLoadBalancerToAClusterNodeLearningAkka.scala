ad0e81def68c6cb3153eefd77ef096b3935a95b6
larinpaul <www.larinpavel@mail.ru>
2024-10-13 10:55:18 +0500

    // package com.packt.akka.loadBalancing // import ...
    class Frontend extends Actor {
      import context.dispatcher
    
      val backend = context.actorOf(FromConfig.props(), name = "backendRouter")
    
      context.system.scheduler.schedule(3.seconds, 3.seconds, self,
        Add(Random.nextInt(100), Random.nextInt(100)))
    
      def receive = {
        case addOp: Add =>
            println("Frontend: I'll forward add operation to backend node to handle it.")
          backend forward addOp
      }
    
    }

--- src/main/scala/section05playingwithremoteactors/Section53AddingLoadBalancerToAClusterNodeLearningAkka.scala
+++ src/main/scala/section05playingwithremoteactors/Section53AddingLoadBalancerToAClusterNodeLearningAkka.scala
@@ -1,5 +1,11 @@
 package section05playingwithremoteactors
 
+import akka.actor.Actor
+import akka.routing.FromConfig
+
+import scala.concurrent.duration.DurationInt
+import scala.util.Random
+
 class Section53AddingLoadBalancerToAClusterNodeLearningAkka {
 
   // We will take a look at:
@@ -18,3 +24,24 @@ class Section53AddingLoadBalancerToAClusterNodeLearningAkka {
   // * CPU utilization
 
 }
+
+// package com.packt.akka.loadBalancing
+// import ...
+
+class Frontend extends Actor {
+  import context.dispatcher
+
+  val backend = context.actorOf(FromConfig.props(), name = "backendRouter")
+
+  context.system.scheduler.schedule(3.seconds, 3.seconds, self,
+    Add(Random.nextInt(100), Random.nextInt(100)))
+
+  def receive = {
+    case addOp: Add =>
+        println("Frontend: I'll forward add operation to backend node to handle it.")
+      backend forward addOp
+  }
+
+}
+
+
