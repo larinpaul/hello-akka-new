7cb6901d88953ea84a362208b0d489dd841c3f20
larinpaul <www.larinpavel@mail.ru>
2024-10-13 10:48:37 +0500

    // We will take a look at:   // * Cluster metrics   // * Introduction to load balancer   // * Add load balancer to our cluster
      // In our cluster we had:
      // 1 frontend node
      // 3 backend nodes
    
      // Cluster metrics report the system status back to Akka
      // Cluster metrics contains data about
      // * Heap memory
      // * System load for the last minute
      // * CPU utilization

--- src/main/scala/section05playingwithremoteactors/Section53AddingLoadBalancerToAClusterNodeLearningAkka.scala
+++ src/main/scala/section05playingwithremoteactors/Section53AddingLoadBalancerToAClusterNodeLearningAkka.scala
@@ -2,4 +2,19 @@ package section05playingwithremoteactors
 
 class Section53AddingLoadBalancerToAClusterNodeLearningAkka {
 
+  // We will take a look at:
+  // * Cluster metrics
+  // * Introduction to load balancer
+  // * Add load balancer to our cluster
+
+  // In our cluster we had:
+  // 1 frontend node
+  // 3 backend nodes
+
+  // Cluster metrics report the system status back to Akka
+  // Cluster metrics contains data about
+  // * Heap memory
+  // * System load for the last minute
+  // * CPU utilization
+
 }
