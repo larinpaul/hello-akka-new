833f71daee66177c513385bf9a105bafea0011f4
larinpaul <www.larinpavel@mail.ru>
2024-10-15 07:39:31 +0500

    // We are going to look at:   // * Cluster Singleton   // * How it works   // * Implementing Singleton Actor
      // Use Cases to Use Singleton
      // * Single point of responsibility for important decision in the cluster
      // * Single entry point to an external
      // * Single master, many workers
      // * Centralized naming service or routing logic
    
      // Singleton should not be your first design choice since it has drawbacks...
      // Drawbacks:
      // * Cluster Singleton may be a bottleneck
      // * Cluster Singleton can not be available all the time

---
+++ src/main/scala/section05playingwithremoteactors/Section54CreatingASingletonActorInTheCluster.scala
@@ -0,0 +1,21 @@
+package section05playingwithremoteactors
+
+class Section54CreatingASingletonActorInTheCluster {
+
+  // We are going to look at:
+  // * Cluster Singleton
+  // * How it works
+  // * Implementing Singleton Actor
+
+  // Use Cases to Use Singleton
+  // * Single point of responsibility for important decision in the cluster
+  // * Single entry point to an external
+  // * Single master, many workers
+  // * Centralized naming service or routing logic
+
+  // Singleton should not be your first design choice since it has drawbacks...
+  // Drawbacks:
+  // * Cluster Singleton may be a bottleneck
+  // * Cluster Singleton can not be available all the time
+
+}
