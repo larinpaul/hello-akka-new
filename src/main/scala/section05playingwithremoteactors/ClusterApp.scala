7d1c832171639a4a830c7f236080edb18ac47c87
larinpaul <www.larinpavel@mail.ru>
2024-10-19 08:41:16 +0500

    object SingletonApp extends App {
      startup(Seq("2551", "2552", "0"))
    
      def startup(ports: Seq[String]): Unit = {
        ports foreach { port =>
    
          // Override the configuration of the port
          val config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
            withFallback(ConfigFactory.load("singleton"))
    
          // Create an Akka system
          val system = ActorSystem("ClusterSystem", config)
    
          startupSharedJournal(system startStore = (port == "2551"), path =
            ActorPath.fromString("akka.tcp://ClusterSystem@127.0.0.1:2551/user/store"))
    
          val master = system.actorOf(ClusterSingletonManager.props(
            SingletonProps = Props[Master],
            terminationMessage = PoisonPill,
            settings = ClusterSingletonManagerSettings(system).withRole(None)
          ), name = "master")
    
          Cluster(system) registerOnMemberUp {
            system.actorOf(Worker.props, name = "worker")
          }
    
          // ...
    
        }
      }
    
    }

--- src/main/scala/section05playingwithremoteactors/ClusterApp.scala
+++ src/main/scala/section05playingwithremoteactors/ClusterApp.scala
@@ -1,5 +1,9 @@
 package section05playingwithremoteactors
 
+import akka.actor.Nobody.path
+import akka.actor.{ActorPath, ActorSystem, PoisonPill, Props}
+import com.typesafe.config.ConfigFactory
+
 class ClusterApp extends App {
 
   //initiate frontend node
@@ -17,3 +21,38 @@ class ClusterApp extends App {
   Frontend.getFrontend ! Add(2, 4)
 
 }
+
+object SingletonApp extends App {
+
+  startup(Seq("2551", "2552", "0"))
+
+  def startup(ports: Seq[String]): Unit = {
+    ports foreach { port =>
+
+      // Override the configuration of the port
+      val config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
+        withFallback(ConfigFactory.load("singleton"))
+
+      // Create an Akka system
+      val system = ActorSystem("ClusterSystem", config)
+
+      startupSharedJournal(system startStore = (port == "2551"), path =
+        ActorPath.fromString("akka.tcp://ClusterSystem@127.0.0.1:2551/user/store"))
+
+      val master = system.actorOf(ClusterSingletonManager.props(
+        SingletonProps = Props[Master],
+        terminationMessage = PoisonPill,
+        settings = ClusterSingletonManagerSettings(system).withRole(None)
+      ), name = "master")
+
+      Cluster(system) registerOnMemberUp {
+        system.actorOf(Worker.props, name = "worker")
+      }
+
+      // ...
+
+    }
+  }
+
+}
+
--- src/main/scala/section05playingwithremoteactors/Section54CreatingASingletonActorInTheCluster.scala
+++ src/main/scala/section05playingwithremoteactors/Section54CreatingASingletonActorInTheCluster.scala
@@ -3,6 +3,7 @@ package section05playingwithremoteactors
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.persistence.PersistentActor
 import akka.persistence.Recovery.none
+import section05playingwithremoteactors.Frontend.Tick
 import section05playingwithremoteactors.Master.{RegisterWorker, RequestWork, Work}
 
 import scala.concurrent.duration.DurationInt
@@ -108,4 +109,29 @@ object Worker {
   def props = Props(new Wokrer())
 }
 
+// package com.packt.akka.cluster.singleton
 
+// import ...
+
+class Frontend extends Actor with ActorLogging {
+  import ...
+
+  val masterProxy = context.actorOf(ClusterSingletonProxy.props(
+    singletonManagerPath = "/user/master",
+    settings = ClusterSingletonProxySettings(context.system).withRole(None)
+  ), name = "masterProxy")
+
+  context.system.scheduler.schedule(0.second, 3.second, self, Tick)
+
+  def receive = {
+    case Tick =>
+      masterProxy ! Master.Work(self, "add")
+  }
+
+}
+
+object Frontend {
+  case object Tick
+
+  def props = Props(new Frontend())
+}
