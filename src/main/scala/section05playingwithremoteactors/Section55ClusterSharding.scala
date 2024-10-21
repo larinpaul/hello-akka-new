3c73e3cfa1349d4abf78c2b0f404bb9713c67b62
larinpaul <www.larinpavel@mail.ru>
2024-10-21 08:31:38 +0500

    package section05playingwithremoteactors
    class Section55ClusterSharding {
    
      // A shard is a group of actors with an identifier
      // and these actors are called entries
    
      // Each node on a cluster involves running an actor called shard region
      // this actor is responsible for managing entries
    
    }

---
+++ src/main/scala/section05playingwithremoteactors/Section55ClusterSharding.scala
@@ -0,0 +1,11 @@
+package section05playingwithremoteactors
+
+class Section55ClusterSharding {
+
+  // A shard is a group of actors with an identifier
+  // and these actors are called entries
+
+  // Each node on a cluster involves running an actor called shard region
+  // this actor is responsible for managing entries
+
+}
