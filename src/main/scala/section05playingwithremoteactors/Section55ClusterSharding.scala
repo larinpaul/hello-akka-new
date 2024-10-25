0ef7846afd37c2ebdbf099b4464823522ed00c08
larinpaul <www.larinpavel@mail.ru>
2024-10-25 09:44:21 +0500

    // Every 3 second will send increment operation   context.system.scheduler.schedule(3.seconds, 3.seconds, self, Tick(Inc))
      // Every 6 second will send Decrement operation
      context.system.scheduler.schedule(6.seconds, 6.seconds, self, Tick(Dec))
    
      // Every 10 second will send get operation
      context.system.scheduler.schedule(10.seconds, 10.seconds, self, Tick(Get))

--- src/main/scala/section05playingwithremoteactors/Section55ClusterSharding.scala
+++ src/main/scala/section05playingwithremoteactors/Section55ClusterSharding.scala
@@ -99,6 +99,15 @@ class Frontend extends Actor with ActorLogging {
 
   val counterRegion: ActorRef = ClusterSharding(context.system).shardRegion(Counter.shardName)
 
+  // Every 3 second will send increment operation
+  context.system.scheduler.schedule(3.seconds, 3.seconds, self, Tick(Inc))
+
+  // Every 6 second will send Decrement operation
+  context.system.scheduler.schedule(6.seconds, 6.seconds, self, Tick(Dec))
+
+  // Every 10 second will send get operation
+  context.system.scheduler.schedule(10.seconds, 10.seconds, self, Tick(Get))
+
   def receive = {
     case Tick(Inc) =>
       log.info(s"Frontend: Send Increment message to shard region.")
