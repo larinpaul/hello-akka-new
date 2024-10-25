2fc09954d63367208f9cc394b98e55051820b837
larinpaul <www.larinpavel@mail.ru>
2024-10-25 09:50:49 +0500

    // import ...
    // This is to start a distributed journal
    
    object ShardingApp extends App {
    
      startup(Seq("2551", "2552", "0"))
    
      def startup(ports: Seq[String]): Unit = {
        ports foreach { port =>
          // Override the configuration of the port
          val config = ConfigFactor.parseString("akka.remote.netty.tcp.port=" + port).
            withFallback(ConfigFactory.load("sharding"))
    
          // Create an Akka system
          val system = ActorSystem("ClusterSystem", config)
    
          startupSharedJournal(system, startStore = (port == "2551"), path =
           ActorPath.fromString("akka.tcp://ClusterSystem@127.0.0.1:2551/user/store"))
    
          ClusterSharding(system).start(
            typeName = Counter.shardName,
            entityProps = Counter.props(),
            settings = ClusterShardingSettings(system),
            extractEntityId = Counter.idExtractor,
            extractShardId = Counter.shardResolver)
    
          if (port != "2551" && port != "2552") {
            Cluster(system) registerOnMemberUp {
              system.actorOf(Props[Frontend], "frontend")
            }
          }
    
        }
      }
    
    }

--- src/main/scala/section05playingwithremoteactors/Section55ClusterSharding.scala
+++ src/main/scala/section05playingwithremoteactors/Section55ClusterSharding.scala
@@ -1,10 +1,12 @@
 package section05playingwithremoteactors
 
 import Counter.Inc
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.actor.{Actor, ActorLogging, ActorPath, ActorRef, ActorSystem, Props}
 import akka.actor.SupervisorStrategy.Stop
 import akka.persistence.PersistentActor
+import com.typesafe.config.ConfigFactory
 import section05playingwithremoteactors.Frontend.{Dec, Get, Inc, Tick}
+import section05playingwithremoteactors.SingletonApp.startup
 
 import scala.concurrent.duration.DurationInt
 import scala.util.Random
@@ -131,3 +133,42 @@ object Frontend {
 
   case class Tick(op: Op)
 }
+
+// import ...
+
+// This is to start a distributed journal
+
+object ShardingApp extends App {
+
+  startup(Seq("2551", "2552", "0"))
+
+  def startup(ports: Seq[String]): Unit = {
+    ports foreach { port =>
+      // Override the configuration of the port
+      val config = ConfigFactor.parseString("akka.remote.netty.tcp.port=" + port).
+        withFallback(ConfigFactory.load("sharding"))
+
+      // Create an Akka system
+      val system = ActorSystem("ClusterSystem", config)
+
+      startupSharedJournal(system, startStore = (port == "2551"), path =
+       ActorPath.fromString("akka.tcp://ClusterSystem@127.0.0.1:2551/user/store"))
+
+      ClusterSharding(system).start(
+        typeName = Counter.shardName,
+        entityProps = Counter.props(),
+        settings = ClusterShardingSettings(system),
+        extractEntityId = Counter.idExtractor,
+        extractShardId = Counter.shardResolver)
+
+      if (port != "2551" && port != "2552") {
+        Cluster(system) registerOnMemberUp {
+          system.actorOf(Props[Frontend], "frontend")
+        }
+      }
+
+    }
+  }
+
+}
+
