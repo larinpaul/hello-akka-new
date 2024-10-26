840be3c6cca0a368d7289c9dedcfb5c7f145e8b8
larinpaul <www.larinpavel@mail.ru>
2024-10-26 09:39:14 +0500

    // In this Section, we are going to take a look at...   // * How to test actors   // * How to test parent-child relationships   // * How to test FSMs   // * How to do Multi Node testing

---
+++ src/main/scala/section6testing/Main.scala
@@ -0,0 +1,11 @@
+package section6testing
+
+class Main {
+
+  // In this Section, we are going to take a look at...
+  // * How to test actors
+  // * How to test parent-child relationships
+  // * How to test FSMs
+  // * How to do Multi Node testing
+
+}
