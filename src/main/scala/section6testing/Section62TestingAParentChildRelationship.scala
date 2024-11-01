5d8a987d67c7e825708bceb9ecf21f1af6b1d406
larinpaul <www.larinpavel@mail.ru>
2024-11-01 09:37:03 +0500

    // package com.packt.akka
    import akka.actor.{ Actor, Props }
    
    class Parent extends Actor {
      val child = context.actorOf(Props[Child])
      var ponged = false
    
      def receive = {
        case "ping" => child ! "ping"
        case "pong" => ponged = true
      }
    }

--- src/main/scala/section6testing/Section62TestingAParentChildRelationship.scala
+++ src/main/scala/section6testing/Section62TestingAParentChildRelationship.scala
@@ -12,9 +12,23 @@ class Section62TestingAParentChildRelationship {
 
 import akka.actor.Actor
 
-class Child extends Actor {
+class Child extends Actor { // When it receives the "ping" message, it sends a "pong" to its parent
   def receive = {
     case "ping" => context.parent ! "pong"
+  }
+}
+
+// package com.packt.akka
+
+import akka.actor.{ Actor, Props }
+
+class Parent extends Actor {
+  val child = context.actorOf(Props[Child])
+  var ponged = false
+
+  def receive = {
+    case "ping" => child ! "ping"
+    case "pong" => ponged = true
   }
 }
 
