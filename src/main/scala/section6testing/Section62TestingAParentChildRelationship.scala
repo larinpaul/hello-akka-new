66d13ac35d0cc48218db32693ecce6a6eb75274e
larinpaul <www.larinpavel@mail.ru>
2024-11-01 09:14:11 +0500

    Creating a new section

---
+++ src/main/scala/section6testing/Section62TestingAParentChildRelationship.scala
@@ -0,0 +1,9 @@
+package section6testing
+
+class Section62TestingAParentChildRelationship {
+
+  // In this section, we are going to look at...
+  // * Testing parent actor
+  // * Testing child actor
+
+}
