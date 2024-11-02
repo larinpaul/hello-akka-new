348c9270422b2f6c86684c612fbbb1893eb50dc6
larinpaul <www.larinpavel@mail.ru>
2024-11-02 07:35:04 +0500

    import akka.actor.{ActorSystem, Props}
    class ChildSpec extends TestKit(ActorSystem("test-system"))
                      with ImplicitSender
                      with FlatSpecLike
                      with BeforeAndAfterAll
                      with MustMatchers {
    
      override def afterAll(): Unit = {
        TestKit.shutdownActorSystem(system)
      }
    
      "Child Actor" should "send pong message when receive ping message" in {
        val parent = TestProbe()
    
        val child = system.actorOf(Props(new Child(parent.ref)))
    
        child ! "ping"
    
        parent.expectMsg("pong")
      }

---
+++ src/test/scala/com/packt/akka/ChildSpec.scala
@@ -0,0 +1,25 @@
+package com.packt.akka
+
+import akka.actor.{ActorSystem, Props}
+
+class ChildSpec extends TestKit(ActorSystem("test-system"))
+                  with ImplicitSender
+                  with FlatSpecLike
+                  with BeforeAndAfterAll
+                  with MustMatchers {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "Child Actor" should "send pong message when receive ping message" in {
+    val parent = TestProbe()
+
+    val child = system.actorOf(Props(new Child(parent.ref)))
+
+    child ! "ping"
+
+    parent.expectMsg("pong")
+  }
+
+}
