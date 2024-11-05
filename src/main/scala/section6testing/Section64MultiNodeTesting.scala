e5248f45d9b7b15f7d89b90bc5f3bfe60612c830
larinpaul <www.larinpavel@mail.ru>
2024-11-05 07:14:56 +0500

    package section6testing
    class Section64MultiNodeTesting {
    
      // Part 6.4 Multi Node Testing
      // * Introduction to Multi Node testing
      // * Parts of Multi Node test kit
      // * Implementing an example
      // * Multi JVM testing (same machine, multiple JVM)
    
    }

--- src/main/scala/section6testing/Section64MultiNodeTesting.scala
+++ src/main/scala/section6testing/Section64MultiNodeTesting.scala
@@ -3,5 +3,9 @@ package section6testing
 class Section64MultiNodeTesting {
 
   // Part 6.4 Multi Node Testing
+  // * Introduction to Multi Node testing
+  // * Parts of Multi Node test kit
+  // * Implementing an example
+  // * Multi JVM testing (same machine, multiple JVM)
 
 }
